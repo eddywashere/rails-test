f015a420ecc9a31f597604730ba05db777970b76
Eddy Hernandez <eddy.hernandez@rackspace.com>
2014-02-20 18:07:28 -0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foobar::Application.config.secret_key_base = '75c7746d2073cec20a1e0090694bf27db88fe709bb3212ce916124ef2452c024ec5fc523cb1f7c21864f7dba34149730295e9f8171456ed795025a43ebbf91b7'
